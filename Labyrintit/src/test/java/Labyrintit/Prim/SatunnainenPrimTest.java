20fe2cc1da669d0bef0e3c13a162371dca630feb
Janne <janne.penttala@helsinki.fi>
2022-10-30 20:28:40 +0200

    testejä

--- Labyrintit/src/test/java/Labyrintit/Prim/SatunnainenPrimTest.java
+++ Labyrintit/src/test/java/Labyrintit/Prim/SatunnainenPrimTest.java
@@ -7,6 +7,58 @@ import static org.junit.Assert.*;
 public class SatunnainenPrimTest {
     
     @Test
+    public void reittiOikeaanYlakulmaan() {
+        SatunnainenPrim prim = new SatunnainenPrim(15,15);
+        int[][] labyrintti = prim.luoLabyrintti();
+        
+        int alkuX = 0;
+        for (int i = 0; i < labyrintti.length; i++) {
+            if (labyrintti[i][1] == 1) {
+                alkuX = i;
+                break;
+            }
+        }
+        
+        boolean[][] taulukko = luoTotuusTaulukko(labyrintti, alkuX, 1);
+        
+        int loppuX = 0;
+        for (int i = labyrintti.length - 1; i >= 0; i--) {
+            if (labyrintti[i][2] == 1) {
+                loppuX = i;
+                break;
+            }
+        }
+        
+        assertEquals(true, taulukko[loppuX][2]);
+    }
+    
+    @Test
+    public void reittiVasempaanAlakulmaan() {
+        SatunnainenPrim prim = new SatunnainenPrim(15,15);
+        int[][] labyrintti = prim.luoLabyrintti();
+        
+        int alkuX = 0;
+        for (int i = 0; i < labyrintti.length; i++) {
+            if (labyrintti[i][1] == 1) {
+                alkuX = i;
+                break;
+            }
+        }
+        
+        boolean[][] taulukko = luoTotuusTaulukko(labyrintti, alkuX, 1);
+        
+        int loppuX = 0;
+        for (int i = 0; i < labyrintti.length; i++) {
+            if (labyrintti[i][labyrintti.length - 3] == 1) {
+                loppuX = i;
+                break;
+            }
+        }
+        
+        assertEquals(true, taulukko[loppuX][labyrintti.length - 3]);
+    }
+    
+    @Test
     public void reittiOikeaanAlakulmaan() {
         SatunnainenPrim prim = new SatunnainenPrim(15,15);
         int[][] labyrintti = prim.luoLabyrintti();
